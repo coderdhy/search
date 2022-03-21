2f626299a66292fff71dc7922ae82695672acc7f
donghongyue <donghongyue@bytedance.com>
2022-03-21 14:39:45 +0800

    target os

--- build.sh
+++ build.sh
@@ -7,5 +7,15 @@ else
   mkdir $buildpath
 fi
 cd $buildpath
-cmake ..
+
+# compile os
+SHELL_OS=$(uname -a | grep -io 'MINGW\|Linux\|Darwin' | head -1 | tr "[:upper:]" "[:lower:]")
+echo "--current shell os $SHELL_OS"
+
+if [ "$SHELL_OS" = "darwin" ]; then
+  cmake .. -G "Xcode"
+else
+  cmake .. -G "Visual Studio 15 2017"
+fi
+
 
